38f9123b4e43f62effff92ef3595867987819c5b
Saumya Pande <saumyapande329@gmail.com>
2023-02-11 16:58:40 +0530

    Changing PrimeNumber

--- src/com/google/Basics/PrimeNumber.java
+++ src/com/google/Basics/PrimeNumber.java
@@ -7,25 +7,22 @@ public class PrimeNumber {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		int num=sc.nextInt();
-		int i, flag=0;
-		
-		if(num==1||num==0)
-			System.out.println("Number is not Prime!");
-		
-		else {
-			for(i=2;i<num/2;i++)
+	
+		for(int i=2;i<=num;i++)
+		{
+			int flag=0;
+			for(int j=2;j<=i/2;j++)
 			{
-				if(num%i==0) {
-					System.out.println("Number is not prime!");
-				    flag=1;
+				if(i%j==0)
+				{
+					flag=1;
 				    break;
 				}
+				
 			}
-			
-			if (flag==0)
-					System.out.println("Number is prime!");
+			if(flag==0)
+				System.out.println(i);
 		}
-
+		
 	}
-
 }
